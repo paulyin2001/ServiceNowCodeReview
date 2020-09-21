68b42023818faf9e4abaced8a512e1836781ad7b
System Administrator <admin@example.com>
2020-09-20 20:44:30 -0500

    Changes by 'System Administrator' on 'ServiceNowCodeReview - 1.0.0' -- Build #1

---
+++ sn/ServiceNowCodeReview/Script Include/active_true/global.TestScriptInclude.js
@@ -0,0 +1,17 @@
+/* 
+ * Application : ServiceNowCodeReview
+ * ClassName   : sys_script_include
+ * Created On  : 2020-09-16 19:46:59
+ * Created By  : admin
+ * Updated On  : 2020-09-16 19:46:59
+ * Updated By  : admin
+ * URL         : /sys_script_include.do?sys_id=ef25e12ddb131010c6ea1fc7689619dc
+ */
+var TestScriptInclude = Class.create();
+TestScriptInclude.prototype = {
+    initialize: function() {
+		gs.info('hello world');
+    },
+
+    type: 'TestScriptInclude'
+};
