3fbfa5e047a0d9b7eca2080790f14b406ad518aa
System Administrator <admin@example.com>
2020-09-22 16:25:49 -0500

    Changes by 'System Administrator' on 'ServiceNowCodeReview - 1.0.3' -- Build #1

--- sn/ServiceNowCodeReview/Client Script/incident/active_true/Show message.js
+++ sn/ServiceNowCodeReview/Client Script/incident/active_true/Show message.js
@@ -3,13 +3,14 @@
  * ClassName   : sys_script_client
  * Created On  : 2020-09-21 16:57:03
  * Created By  : admin
- * Updated On  : 2020-09-21 19:48:45
+ * Updated On  : 2020-09-22 21:24:28
  * Updated By  : admin
  * URL         : /sys_script_client.do?sys_id=e33e664fdb131010c6ea1fc768961912
  */
 function onLoad() {
    //Type appropriate comment here, and begin script below
-    alert("Hello World");
+    alert("Hello Rolandas!");
 	alert('I can add to a new update set in the global scope!');
 	//failed. maybe just to make sure the scope is correct?
+	//add some comments
 }
