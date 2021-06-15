485d61c8e8000e23f8a71b1d572ab0f134602afc
System Administrator <admin@example.com>
2021-06-15 14:56:04 -0700

    Changes by 'System Administrator' on 'ServiceNowCodeReview - 1.0.0' -- Build #1

---
+++ sn/ServiceNowCodeReview/Script Include/active_true/global.testSI.js
@@ -0,0 +1,16 @@
+/* 
+ * Application : ServiceNowCodeReview
+ * ClassName   : sys_script_include
+ * Created On  : 2021-06-15 20:36:43
+ * Created By  : admin
+ * Updated On  : 2021-06-15 20:36:43
+ * Updated By  : admin
+ * URL         : /sys_script_include.do?sys_id=1301e2051bf03010625cdc6cdc4bcb3c
+ */
+var testSI = Class.create();
+testSI.prototype = {
+    initialize: function() {
+    },
+
+    type: 'testSI'
+};
