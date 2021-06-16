f70d383ca41465e1c0e26e521450cf5760afb12d
System Administrator <admin@example.com>
2021-06-16 10:51:34 -0700

    Changes by 'System Administrator' on 'ServiceNowCodeReview - 1.0.3' -- Build #1

---
+++ sn/ServiceNowCodeReview/Client Script/sys_update_set/active_true/basic alert.js
@@ -0,0 +1,13 @@
+/* 
+ * Application : ServiceNowCodeReview
+ * ClassName   : sys_script_client
+ * Created On  : 2021-06-16 17:44:59
+ * Created By  : admin
+ * Updated On  : 2021-06-16 17:51:02
+ * Updated By  : admin
+ * URL         : /sys_script_client.do?sys_id=014383151b703010625cdc6cdc4bcbb1
+ */
+function onLoad() {
+   //Type appropriate comment here, and begin script below
+   alert('hello world!');
+}
