e095c3080080af59db58b8a69f0c123c61b4e9fd
System Administrator <admin@example.com>
2021-06-17 15:06:24 -0700

    Changes by 'System Administrator' on 'ServiceNowCodeReview - 1.0.0' -- Build #1

---
+++ sn/ServiceNowCodeReview/Scripted REST Resource/TestRESTAPi/active_true/retrieve-GET.js
@@ -0,0 +1,14 @@
+/* 
+ * Application : ServiceNowCodeReview
+ * ClassName   : sys_ws_operation
+ * Created On  : 2021-06-17 21:55:55
+ * Created By  : admin
+ * Updated On  : 2021-06-17 21:55:55
+ * Updated By  : admin
+ * URL         : /sys_ws_operation.do?sys_id=c056c1392f743010717ade1a2799b6b1
+ */
+(function process(/*RESTAPIRequest*/ request, /*RESTAPIResponse*/ response) {
+
+    // implement resource here
+	gs.info('Hello world');
+})(request, response);
